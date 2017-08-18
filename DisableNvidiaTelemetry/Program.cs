12a38cbdee77e0bafa0ab8dbe00fcdb459715067
Nate Shoffner <nate.shoffner@gmail.com>
2017-08-18 16:58:31 -0400

    Silent mode

--- DisableNvidiaTelemetry/Program.cs
+++ DisableNvidiaTelemetry/Program.cs
@@ -2,17 +2,21 @@
 
 using System;
 using System.IO;
+using System.Linq;
 using System.Security.Principal;
 using System.Windows.Forms;
 using DisableNvidiaTelemetry.Forms;
 using DisableNvidiaTelemetry.Utilities;
+using log4net.Core;
 
 #endregion
 
 namespace DisableNvidiaTelemetry
 {
-    static class Program
+    internal static class Program
     {
+        public const string StartupParamSilent = "-silent";
+
         [STAThread]
         private static void Main(string[] args)
         {
@@ -25,22 +29,50 @@ namespace DisableNvidiaTelemetry
             // log all the errors
             AppDomain.CurrentDomain.UnhandledException += (s, e) =>
             {
-                var ex = (Exception)e.ExceptionObject;
+                var ex = (Exception) e.ExceptionObject;
                 Logging.GetLogger().Error(ex);
             };
+
+            var silentMode = false;
+
+            if (args.Length > 0)
+            {
+                Logging.GetLogger().Log(Level.Info, $"Startup params: {string.Join(" ", args)}");
+
+                if (args.Contains(StartupParamSilent))
+                {
+                    silentMode = true;
+                    SilentlyDisableTelemetry();
+                }
+            }
 
             if (!IsAdministrator())
             {
                 MessageBox.Show("Please run the program as administrator to continue.", "Administrator Required", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
 
-            Application.Run(new FormMain());
+            if (!silentMode)
+            {
+                Application.Run(new FormMain());
+            }
         }
-
         private static bool IsAdministrator()
         {
             return (new WindowsPrincipal(WindowsIdentity.GetCurrent()))
                 .IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        private static void SilentlyDisableTelemetry()
+        {
+            Logging.GetLogger().Log(Level.Info, "Silently disabling telemetry services.");
+
+            var services = NvidiaController.GetTelemetryServices(true);
+            NvidiaController.DisableTelemetryServices(services.Select(s => s.Service).ToList());
+
+            Logging.GetLogger().Log(Level.Info, "Silently disabling telemetry tasks.");
+
+            var tasks = NvidiaController.GetTelemetryTasks(true);
+            NvidiaController.DisableTelemetryTasks(tasks.Select(t => t.Task).ToList());
         }
     }
 }

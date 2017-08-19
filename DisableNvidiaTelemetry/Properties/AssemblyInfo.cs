bb9001913575a587515e341417ca79a608936ce0
Nate Shoffner <nate.shoffner@gmail.com>
2017-08-19 08:54:30 -0400

    Version bump

--- DisableNvidiaTelemetry/Properties/AssemblyInfo.cs
+++ DisableNvidiaTelemetry/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nate Shoffner")]
 [assembly: AssemblyProduct("Disable Nvidia Telemetry")]
-[assembly: AssemblyCopyright("Copyright ©  Nate Shoffner 2017")]
+[assembly: AssemblyCopyright("Copyright © Nate Shoffner 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]

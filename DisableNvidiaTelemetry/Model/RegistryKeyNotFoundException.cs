5197de9e05cb15e80fef3d0c84899859b098c51d
Sleepydragn1 <sleepydragon10@hotmail.com>
2018-10-03 07:27:45 -0500

    Included a missing file that should've been obvious, thus fixing the check...

---
+++ DisableNvidiaTelemetry/Model/RegistryKeyNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DisableNvidiaTelemetry.Model
+{
+    /// <summary> Represents an exception where a registry key could not be found. </summary>
+    public class RegistryKeyNotFoundException : Exception
+    {
+        public RegistryKeyNotFoundException()
+        {
+        }
+
+        public RegistryKeyNotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
